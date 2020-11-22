6a4b41658113ccdeb43c1f8dda4a4b0e949d9c7f
Katerina Metaxopoulou <kat@katmetax.com>
2020-11-22 15:47:02 +0000

    Innit commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>O SOUND</Text>
       <StatusBar style="auto" />
+      {/* <Text>Choose your colour:</Text>
+      <Button></Button> */}
     </View>
   );
 }
@@ -14,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
